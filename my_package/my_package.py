f82b36bf31ac5ac4feefabe417c5c13389017004
Brian Frost <frosty@Brians-MacBook-Pro.local>
2025-02-03 11:45:24 -0800

    Update docstrings

--- my_package/my_package.py
+++ my_package/my_package.py
@@ -1,12 +1,39 @@
 class MyPackage:
+    """
+    A sample package demonstrating Sphinx documentation.
+
+    Attributes
+    ----------
+    testing : str
+        A sample attribute initialized to `"yeah"`.
+    """
+
     def __init__(self):
+        """
+        Initializes the MyPackage class.
+        """
         self.testing = "yeah"
 
     def my_func(self):
+        """
+        A placeholder function for future functionality.
+
+        This function currently does nothing.
+        """
         pass
 
     def new_func(self):
+        """
+        Another placeholder function for new features.
+
+        This function currently does nothing.
+        """
         pass
 
     def another_func(self):
+        """
+        A third placeholder function.
+
+        This function currently does nothing.
+        """
         pass
